dee32e64097be0e58ff4520368ba3ea79374c582
mcampbellr <8326907+mcampbellr@users.noreply.github.com>
2021-11-30 15:43:55 -0600

    Handle the error when the user cancel the prompt

--- vimtheme.go
+++ vimtheme.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 )
 
 var readLine = bufio.NewReader(os.Stdin)
 
 func main() {
-	selectedTheme, _ := selectTheme()
+	selectedTheme, err := selectTheme()
+
+	if err != nil {
+		fmt.Println("Theme not changed: ", err)
+		return
+	}
+
 	mainSearch := `colorscheme\s[a-zA-Z_]+`
 	mainReplace := "colorscheme " + selectedTheme.Theme
 	mainFileName := "nvim/init.vim"
